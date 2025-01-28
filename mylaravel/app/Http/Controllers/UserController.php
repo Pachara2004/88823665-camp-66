cc89d5a2b1cd6bca8f5a1e8ba2473e0ab5c819da
Pachara2004 <pachara000004@gmail.com>
2025-01-28 19:23:08 +0700

    update

--- mylaravel/app/Http/Controllers/UserController.php
+++ mylaravel/app/Http/Controllers/UserController.php
@@ -15,5 +15,19 @@ class UserController extends Controller
         return view('user', ['users' => $users]);
     }
 
+    function edit($id){
+        $user = User::find($id);
+        return view('user_edit', ['users' => $user]);
+    }
 
+    function edit_action(Request $req){
+        $user = User::find($req->id);
+        $user->name = $req->name;
+        $user->email = $req->email;
+        $user->password = $req->password;
+        $user->save();
+
+
+        return view('user_edit', ['users' => $user]);
+    }
 }
